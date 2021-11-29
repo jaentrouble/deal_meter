99c3136265301dda5c347b36e7c0d3bff174a6e6
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-11-29 16:49:05 +0900

    building  ui

--- video_labeler/video_labeler.py
+++ video_labeler/video_labeler.py
@@ -10,7 +10,7 @@ class Console():
         self.mainframe.grid(column=0,row=0)
 
         self.label_image = ttk.Label(self.mainframe)
-        self.label_image.grid(column=0,row=0,columnspan=2)
+        self.label_image.grid(column=0,row=0,columnspan=2,rowspan=3)
 
         # TODO : Fill commands
         self.button_cut = ttk.Button(
@@ -18,12 +18,36 @@ class Console():
             text='Cut',
             # command=
         )
-        self.button_cut.grid(column=0, row=1)
+        self.button_cut.grid(column=0, row=3)
         
         self.button_reset = ttk.Button(
             self.mainframe,
             text='Reset',
             # command=
         )
-        self.button_reset.grid(column=1)
+        self.button_reset.grid(column=1, row=3)
+
+        self.radio_var = tk.IntVar(value=0)
+
+        self.radio_nonlabel = ttk.Radiobutton(
+            self.mainframe,
+            text='Not-labelable',
+            variable=self.radio_var,
+            value=0,
+        )
+        self.radio_nonlabel.grid(column=2,row=0)
+        
+        self.radio_label = ttk.Radiobutton(
+            self.mainframe,
+            text='',
+            variable=self.radio_var,
+            value=1,
+        )
+        self.radio_label.grid(column=2,row=1)
+
+        self.entry_var = tk.StringVar()
+        self.entry_label = ttk.Entry(
+            self.mainframe,
+            textvariable=self.entry_var,
+        )
         
