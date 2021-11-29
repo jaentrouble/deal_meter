992dfc09ac08062ab39b51c90bd298e6441cb70b
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-11-29 11:14:40 +0900

    1129

--- video_labeler/video_labeler.py
+++ video_labeler/video_labeler.py
@@ -1,8 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import ImageTk, Image
 
 class Console():
     def __init__(self) -> None:
         self.root = tk.Tk()
         self.root.title('Video Labeler')
+        self.mainframe = ttk.Frame(self.root)
+        self.mainframe.grid(column=0,row=0)
+
+        self.label_image = ttk.Label(self.mainframe)
+        self.label_image.grid(column=0,row=0,columnspan=2)
+
+        # TODO : Fill commands
+        self.button_cut = ttk.Button(
+            self.mainframe,
+            text='Cut',
+            # command=
+        )
+        self.button_cut.grid(column=0, row=1)
+        
+        self.button_reset = ttk.Button(
+            self.mainframe,
+            text='Reset',
+            # command=
+        )
+        self.button_reset.grid(column=1)
         
