6fabafbaf2f7c97c0efc2759fc6062521ec50906
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-12-11 22:34:07 +0900

    lstm model

--- hp_reader/deal_models.py
+++ hp_reader/deal_models.py
@@ -2,11 +2,24 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import layers, applications
 
-def mobv3_lstm(input_shape, digits:int):
-    mobv3 = applications.MobileNetV3Large(
-        input_shape=input_shape,
+def mobv3_lstm(inputs, digits:int):
+    # Before Softmax activation
+    mobv3_output = applications.MobileNetV3Large(
+        input_tensor=inputs,
         weights=None,
         include_top=True,
         classes=1024,
-    )
+    ).get_layer(index=-2).output
+    tiled_output = tf.tile(tf.expand_dims(
+        mobv3_output,axis=1,
+    ),[1,digits,1])
+    lstm_output = layers.LSTM(
+        units=1024,
+        return_sequences=True,
+    )(tiled_output)
+    logit_output = layers.Dense(
+        units=10,
+    )(lstm_output)
+    return logit_output
     
+
