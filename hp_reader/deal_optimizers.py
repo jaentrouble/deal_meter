c90597d3bdb5c19dd0d6196deebfed249419248a
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-12-22 01:21:10 +0900

    wrong decay step

--- hp_reader/deal_optimizers.py
+++ hp_reader/deal_optimizers.py
@@ -12,7 +12,7 @@ exp_3_5_wrong = optimizers.Adam(
 exp_4_6 = optimizers.Adam(
     learning_rate=optimizers.schedules.ExponentialDecay(
         initial_learning_rate=1e-4,
-        decay_steps=1e-6,
+        decay_steps=100,
         decay_rate=0.99,
     )
 )
