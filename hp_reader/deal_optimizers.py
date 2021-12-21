eba621f532a361fca11bc0f4b6b88c5c2cf9901c
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-12-22 01:15:56 +0900

    lower lr

--- hp_reader/deal_optimizers.py
+++ hp_reader/deal_optimizers.py
@@ -1,9 +1,18 @@
 from tensorflow.keras import optimizers
 
-exp_3_5 = optimizers.Adam(
+# Wrong rate
+exp_3_5_wrong = optimizers.Adam(
     learning_rate=optimizers.schedules.ExponentialDecay(
         initial_learning_rate=10e-3,
         decay_steps=10e4,
+        decay_rate=0.99,
+    )
+)
+
+exp_4_6 = optimizers.Adam(
+    learning_rate=optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=1e-4,
+        decay_steps=1e-6,
         decay_rate=0.99,
     )
 )
--- hp_reader/model_trainer.py
+++ hp_reader/model_trainer.py
@@ -241,7 +241,7 @@ if __name__ == '__main__':
 
     kwargs['name'] = args.name
     kwargs['model_function'] = mobv3_lstm
-    kwargs['optimizer'] = exp_3_5
+    kwargs['optimizer'] = exp_4_6
     kwargs['epochs'] = int(args.epochs)
     kwargs['batch_size'] = 128
     kwargs['train_dir_list'] = train_dirs
