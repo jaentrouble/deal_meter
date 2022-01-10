ac8b059f534b10a898134248927fb3f1bbc8c102
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2022-01-11 00:51:17 +0900

    profile?

--- hp_reader/hp_bar_vid_generator.py
+++ hp_reader/hp_bar_vid_generator.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
         'cq:v' : '18',
         'video_bitrate' : '3K',
         'preset' : 'slow',
+        'profile:v' : 'medium',
         'r' : '60',
         's' : f'{width}x{height}',
     }
