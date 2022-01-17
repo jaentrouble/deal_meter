2c1d187ef5371c4aad56ee86fb6d45b521fe0580
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2022-01-18 02:45:12 +0900

    hpbar reader

---
+++ hp_reader/assets/presets.py
@@ -0,0 +1,45 @@
+"""
+presets
+
+All size tuples are in order of (width, height)
+"""
+
+class Preset():
+    def __init__(self, 
+        width:int,
+        height:int,
+        icon_pos:list[tuple[int,int]],
+        icon_name:str,
+        icon_size:tuple[int,int],
+        hpbar_pos:list[tuple[int,int]],
+    ):
+        self.width = width
+        self.height = height
+        self.icon_pos = icon_pos
+        self.icon_pos_hw = [
+            (icon_pos[0][1],icon_pos[0][0]),
+            (icon_pos[1][1],icon_pos[1][0])
+        ]
+        self.icon_name = icon_name
+        self.icon_size = icon_size
+        self.icon_size_hw = (icon_size[1],icon_size[0])
+        self.hpbar_pos = hpbar_pos
+        self.hpbar_pos_hw = [
+            (hpbar_pos[0][1],hpbar_pos[0][0]),
+            (hpbar_pos[1][1],hpbar_pos[1][0])
+        ]
+        self.hp_h_st = hpbar_pos[0][1]
+        self.hp_h_ed = hpbar_pos[1][1]
+        hp_w = hpbar_pos[1][0] - hpbar_pos[0][0]
+        self.hp_w_st = hpbar_pos[0][0] + int(hp_w*0.27)
+        self.hp_w_ed = hpbar_pos[0][0] + int(hp_w*0.70)
+
+
+UHD110COM = Preset(**{
+    'width':3840,
+    'height':2160,
+    'icon_pos':[(1065, 120),(1170,210)],
+    'icon_name':'assets/uhd_110.png',
+    'icon_size':(105,90),
+    'hpbar_pos':[(1220,135),(2660,190)]
+})
---
+++ hp_reader/hp_bar_reader.py
@@ -0,0 +1,62 @@
+from assets import presets
+from model_trainer import deal_model
+import cv2
+import json
+import tensorflow as tf
+import tensorflow.keras as keras
+from skimage.metrics import structural_similarity as ssim
+import numpy as np
+
+DIGITS = 11
+INPUT_SIZE = (640,64)
+
+def hp_logger(
+    vid_path,
+    preset:presets.Preset,
+    model_path,
+):
+    hp_model = keras.models.load_model(model_path)
+    cap = cv2.VideoCapture(vid_path)
+    icon = cv2.imread(preset.icon_name)
+    ipos_1 = preset.icon_pos_hw[0]
+    ipos_2 = preset.icon_pos_hw[1]
+    hp_h_st = preset.hp_h_st
+    hp_h_ed = preset.hp_h_ed
+    hp_w_st = preset.hp_w_st
+    hp_w_ed = preset.hp_w_ed
+    hp_log = []
+    digit_mul = 10**np.arange(DIGITS-1,-1,-1)
+
+    f = 0 # frame count
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        fr_icon = frame[ipos_1[0]:ipos_2[0],ipos_1[1]:ipos_2[1]]
+        if ssim(icon, fr_icon, channel_axis=2)<0.85:
+            continue
+        fr_hpbar = frame[hp_h_st:hp_h_ed,hp_w_st:hp_w_ed,:]
+        fr_hpbar = cv2.resize(fr_hpbar, INPUT_SIZE)
+        fr_hpbar = fr_hpbar[np.newaxis,:,:,:].astype(np.float32)
+        out_vector = np.argmax(hp_model(fr_hpbar, training=False)[0],axis=-1)
+        hp_pred = np.sum(digit_mul*out_vector)
+        hp_log.append((f,hp_pred))
+        f += 1
+    
+    with open(vid_path+'.log', 'w') as l:
+        json.dump(hp_log, l)
+
+if __name__ =='__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v','--video',dest='video')
+    args = parser.parse_args()
+
+    hp_logger(
+        args.video,
+        presets.UHD110COM,
+        'savedmodels/final'
+    )
+    
